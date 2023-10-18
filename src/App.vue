fca583566eb0a5f896ce47ed513ba2b6d69f4141
Eugene Park <parkeugene7676@gmail.com>
2023-10-18 10:07:41 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1 class="text-5xl text-amber-900">Todo List</h1>
+
+    <form>
+      <input class="bg-gray-50 border"/>
+      <button>Add Todo</button>
+      <button>Remove All Todos</button>
+    </form>
+  </div>
+
+  <div>
+    <h1>Done Homework</h1>
+    <h1 class="completed">Riding a Bicycle</h1>
+  </div>
+</template>
+<script setup></script>
+<style>
+.completed
+{
+  text-decoration: line-through;
+}
+</style>
